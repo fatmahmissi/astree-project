1def5708699a299e67671f33d53afacab03010ea
fatmahmissi <fatmahmissi313@gmail.com>
2026-07-22 23:13:46 +0100

    dernier modifi

--- rag-service/app.py
+++ rag-service/app.py
@@ -145,9 +145,9 @@ def get_models():
 
         embedding_model = SentenceTransformer(
             EMBEDDING_MODEL_NAME,
-            device="cpu"
+            device="cpu",
+            backend="onnx"
         )
-
         print("Connexion a ChromaDB...")
 
         chroma_client = chromadb.PersistentClient(
